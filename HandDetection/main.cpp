9701c13f69506b54263fb51cc5a6a82567542c30
Akira Kumashiro <34697080+akira-kumashiro@users.noreply.github.com>
2019-02-22 09:18:09 +0900

    こみっと

--- HandDetection/main.cpp
+++ HandDetection/main.cpp
@@ -6,6 +6,8 @@
 #include <opencv/highgui.h>
 #include <iostream>
 
+#define _ORIGINAL
+
 #ifdef _DEBUG
 //Debugモードの場合
 #pragma comment(lib,"C:\\opencv\\opencv-2.4.11\\build\\x64\\vc12\\lib\\opencv_core2411d.lib")
@@ -391,10 +393,20 @@ bool handDetect(cv::Mat image)//引用元がC言語で書いてるのでここだけC言語
 
 	IplImage* src = cvCreateImage(sz, IPL_DEPTH_8U, 3);
 	//IplImage* gray = cvCreateImage(cvSize(270, 270), 8, 1);
-
 	IplImage* gray = cvCreateImage(sz, IPL_DEPTH_8U, 1);
 
-	*src = image;
+	//*src = image;//このコードだったらメモリが増えるから下のコードで
+	for (int y = 0; y < image.rows; y++)
+	{
+		cv::Vec3b *imagePtr = image.ptr<cv::Vec3b>(y);
+		for (int x = 0; x < image.cols; x++)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				src->imageData[src->widthStep * y + x * 3 + i] = imagePtr[x][i];
+			}
+		}
+	}
 
 	//cvSetImageROI(src, cvRect(340, 100, 270, 270));
 	cvCvtColor(src, gray, CV_BGR2GRAY);
@@ -409,6 +421,7 @@ bool handDetect(cv::Mat image)//引用元がC言語で書いてるのでここだけC言語
 	double area, max_area = 0.0;
 	CvSeq* ptr = 0;
 	//int maxn=0,n=0;
+
 	if (cn > 0)
 	{
 		for (ptr = first_contour; ptr != NULL; ptr = ptr->h_next)
@@ -516,8 +529,8 @@ bool handDetect(cv::Mat image)//引用元がC言語で書いてるのでここだけC言語
 			}
 			cvReleaseMemStorage(&storage1);
 			cvReleaseMemStorage(&storage2);
-			cvClearSeq(ptseq);
-			cvClearSeq(hull);
+			/*cvClearSeq(ptseq);
+			cvClearSeq(hull);*/
 			//cvClearSeq(defects);//エラーが出る
 		}
 	}
@@ -526,6 +539,7 @@ bool handDetect(cv::Mat image)//引用元がC言語で書いてるのでここだけC言語
 	c = cvWaitKey(100);
 
 	cvReleaseImage(&gray);
+	cvReleaseImage(&src);
 	//cvClearSeq(first_contour);
 	//cvClearSeq(maxitem);//エラーが出る
 	//cvClearSeq(ptr);//エラーが出る
@@ -539,6 +553,7 @@ bool handDetect(cv::Mat image)//引用元がC言語で書いてるのでここだけC言語
 
 int main()
 {
+#ifndef _ORIGINAL
 	int c = 0;
 	cv::VideoCapture capture(0);
 	if (capture.isOpened())
@@ -571,4 +586,193 @@ int main()
 
 	cv::destroyAllWindows();
 	return 0;
+#else
+	int c = 0;
+
+	cv::VideoCapture capture(2);
+	cv::Mat img;
+	if (capture.isOpened())
+	{
+		cout << "Video camera capture status: OK" << endl;
+	}
+	else
+	{
+		cout << "Video capture failed, please check the camera." << endl;
+		return -1;
+	}
+
+	/*CvCapture* capture = cvCaptureFromCAM(0);
+
+	if (!cvQueryFrame(capture))
+	{
+		cout << "Video camera capture status: OK" << endl;
+	}
+
+	else
+	{
+		cout << "Video capture failed, please check the camera." << endl;
+	}
+
+	CvSize sz = cvGetSize(cvQueryFrame(capture));
+
+	cout << "Height & width of captured frame: " << sz.height << " x " << sz.width;*/
+
+	capture >> img;
+	CvSize sz = cvSize(img.cols, img.rows);
+	IplImage* src = cvCreateImage(sz, 8, 3);
+	//IplImage* gray = cvCreateImage(cvSize(270, 270), 8, 1);
+	IplImage* gray = cvCreateImage(sz, 8, 1);
+
+	while (c != 27)
+	{
+		capture >> img;
+		*src = img;
+		//src = cvQueryFrame(capture);
+		//cvSetImageROI(src, cvRect(340, 100, 270, 270));
+		cvCvtColor(src, gray, CV_BGR2GRAY);
+		cvSmooth(gray, gray, CV_BLUR, (12, 12), 0);
+		cvNamedWindow("Blur", 1); cvShowImage("Blur", gray);   // blur-not-clear
+		cvThreshold(gray, gray, 0, 255, (CV_THRESH_BINARY_INV + CV_THRESH_OTSU));
+		cvNamedWindow("Threshold", 1); cvShowImage("Threshold", gray);  // black-white
+		CvMemStorage* storage = cvCreateMemStorage();
+		CvSeq* first_contour = NULL;
+		CvSeq* maxitem = NULL;
+		int cn = cvFindContours(gray, storage, &first_contour, sizeof(CvContour), CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+		double area, max_area = 0.0;
+		CvSeq* ptr = 0;
+		//int maxn=0,n=0;
+
+		if (cn > 0)
+		{
+			for (ptr = first_contour; ptr != NULL; ptr = ptr->h_next)
+			{
+				area = fabs(cvContourArea(ptr, CV_WHOLE_SEQ, 0));
+
+				if (area > max_area)
+				{
+					max_area = area;
+					maxitem = ptr;
+					//maxn=n;
+				}
+				// n++;
+			}
+
+			if (max_area > 1000)
+			{
+				CvPoint pt0;
+				CvMemStorage* storage1 = cvCreateMemStorage();
+				CvMemStorage* storage2 = cvCreateMemStorage(0);
+				CvSeq* ptseq = cvCreateSeq(CV_SEQ_KIND_GENERIC | CV_32SC2, sizeof(CvContour), sizeof(CvPoint), storage1);
+				CvSeq* hull;
+				CvSeq* defects;
+
+				for (int i = 0; i < maxitem->total; i++)
+				{
+					CvPoint* p = CV_GET_SEQ_ELEM(CvPoint, maxitem, i);
+					pt0.x = p->x;
+					pt0.y = p->y;
+					cvSeqPush(ptseq, &pt0);
+				}
+
+				hull = cvConvexHull2(ptseq, 0, CV_CLOCKWISE, 0);
+				int hullcount = hull->total;
+				defects = cvConvexityDefects(ptseq, hull, storage2);
+				// pt0 = **CV_GET_SEQ_ELEM( CvPoint*, hull, hullcount - 1 );
+				// printf("** : %d :**",hullcount);
+				CvConvexityDefect* defectArray;
+				// int j=0;
+
+				for (int i = 1; i <= hullcount; i++)
+				{
+					CvPoint pt = **CV_GET_SEQ_ELEM(CvPoint*, hull, i);
+					cvLine(src, pt0, pt, CV_RGB(255, 0, 0), 1, CV_AA, 0);
+					pt0 = pt;
+				}
+
+				for (; defects; defects = defects->h_next)
+				{
+					int nomdef = defects->total; // defect amount
+					// outlet_float( m_nomdef, nomdef );
+					// printf(" defect no %d \n",nomdef);
+					if (nomdef == 0)
+						continue;
+
+					// Alloc memory for defect set.
+					// fprintf(stderr,"malloc\n");
+					defectArray = (CvConvexityDefect*)malloc(sizeof(CvConvexityDefect)*nomdef);
+					// Get defect set.
+					// fprintf(stderr,"cvCvtSeqToArray\n");
+					cvCvtSeqToArray(defects, defectArray, CV_WHOLE_SEQ);
+					// Draw marks for all defects.
+
+					int con = 0;
+
+					for (int i = 0; i < nomdef; i++)
+					{
+						if (defectArray[i].depth > 40)
+						{
+							con = con + 1;
+							// printf(" defect depth for defect %d %f \n",i,defectArray[i].depth);
+							cvLine(src, *(defectArray[i].start), *(defectArray[i].depth_point), CV_RGB(255, 255, 0), 1, CV_AA, 0);
+							cvCircle(src, *(defectArray[i].depth_point), 5, CV_RGB(0, 0, 255), 2, 8, 0);
+							cvCircle(src, *(defectArray[i].start), 5, CV_RGB(0, 255, 0), 2, 8, 0);
+							cvLine(src, *(defectArray[i].depth_point), *(defectArray[i].end), CV_RGB(0, 255, 255), 1, CV_AA, 0);
+							cvDrawContours(src, defects, CV_RGB(0, 0, 0), CV_RGB(255, 0, 0), -1, CV_FILLED, 8);
+						}
+					}
+
+					cvDrawContours(src, maxitem, CV_RGB(255, 0, 0), CV_RGB(128, 128, 128), 0, CV_FILLED, 8);
+
+					// cout<<con<<"\n";
+
+					char txt[40] = "";
+
+					if (con == 1)
+					{
+						char txt1[] = "Hi , This is Udit";
+						strcat(txt, txt1);
+					}
+					else if (con == 2)
+					{
+						char txt1[] = "3 Musketeers";
+						strcat(txt, txt1);
+					}
+					else if (con == 3)
+					{
+						char txt1[] = "Fanatastic 4";
+						strcat(txt, txt1);
+					}
+					else if (con == 4)
+					{
+						char txt1[] = "It's 5";
+						strcat(txt, txt1);
+					}
+					else
+					{
+						char txt1[] = "Jarvis is busy :P"; // Jarvis can't recognise you
+						strcat(txt, txt1);
+					}
+
+					cvNamedWindow("contour", 1); cvShowImage("contour", src);
+					cvResetImageROI(src);
+					CvFont font;
+					cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 1.5, 1.5, 0, 5, CV_AA);
+					cvPutText(src, txt, cvPoint(50, 50), &font, cvScalar(0, 0, 255, 0));
+					// j++;  
+					// Free memory.
+					free(defectArray);
+				}
+				cvReleaseMemStorage(&storage1);
+				cvReleaseMemStorage(&storage2);
+			}
+		}
+
+		cvReleaseMemStorage(&storage);
+		cvNamedWindow("threshold", 1); cvShowImage("threshold", src);
+		c = cvWaitKey(100);
+	}
+
+	//cvReleaseCapture(&capture);
+	cvDestroyAllWindows();
+#endif
 }
